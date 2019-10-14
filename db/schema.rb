bcd1acb7ff913f7fe429fc6d35fc3db71fb5e035
key0123456789 <k.i-0123456789@live.jp>
2019-10-14 23:47:24 +0900

    add limit 30 to tasks name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_140926) do
+ActiveRecord::Schema.define(version: 2019_10_14_144331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
