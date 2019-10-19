ccf0af6322243744c173fe808b66dfec0ae910f7
key0123456789 <k.i-0123456789@live.jp>
2019-10-19 13:56:34 +0900

    User has many tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_165930) do
+ActiveRecord::Schema.define(version: 2019_10_19_041635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
